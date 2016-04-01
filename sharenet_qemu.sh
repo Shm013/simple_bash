66cd148e02d2b4c6b57c8eb761afd9d815c71de5
Nikolay Shamanovich <shm013@yandex.ru>
2016-04-01 18:35:42 +0300

    Add sharenet_qemu.sh script.
    Allow using internet in QEMU mv using bridge device.

---
+++ sharenet_qemu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# sharenet_qemu.sh
+
+qdev="br0"              # bridge device for qemu
+netdev="wlan0"          # device with internet
+
+qnetwork="10.0.2.0/8"   # qemu network
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o $netdev -s $qnetwork -j MASQUERADE
+iptables -I FORWARD 1 -i $qdev -j ACCEPT
+iptables -I FORWARD 1 -o $qdev -m state --state RELATED,ESTABLISHED -j ACCEPT
+echo "share internet from $netdev to $qdev in $qnetwork"
