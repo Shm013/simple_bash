58f0d20aee3cb93c46cb30acbd98e93c098e50c2
Nikolay <shm013@yandex.ru>
2016-12-21 23:15:52 +0300

    Add keyboard switch for lock

--- ck-shutdown.sh
+++ ck-shutdown.sh
@@ -22,6 +22,12 @@ suspend)
     org.freedesktop.UPower.Suspend
     ;;
 lock)
+    setxkbmap us -option # claer xkb option
+
     xscreensaver-command -lock
+    
+    setxkbmap "us,ru" -variant "winkeys" -model pc105 -option "grp:alt_shift_toggle"
+    # Magic! Magic everywhere! (Fix rus keybord in awesome).
+    xkbcomp $DISPLAY - | egrep -v "group . = AltGr;" | xkbcomp - $DISPLAY
     ;;
 esac
