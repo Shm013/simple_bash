ca120b9a1058cc4abe16c76bcb8cfeb9889840e9
Nikolay Shamanovich <shm0xd@gmail.com>
2022-04-06 16:39:58 +0300

    Tweak timeout

--- colormed.sh
+++ colormed.sh
@@ -2,7 +2,7 @@
 
 for x in {1..4}; do
     ffplay -f lavfi -i "sine=frequency=1500:duration=0.2" -autoexit -nodisp
-    sleep 1
+    sleep $(( 2*60 ))
 done
 
 ffplay -f lavfi -i "sine=frequency=1000:duration=0.5" -autoexit -nodisp
